e7abf6aee7a5045ac62585a0e0dbcf281363ae6c
Nikita Teplitskiy <nteplitskiy@localhost.localdomain>
2020-05-08 15:39:21 -0400

    initial server commit

---
+++ FinalApp/server.py
@@ -0,0 +1,35 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from urlparse import parse_qs
+import cgi
+en
+class GP(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+    def do_HEAD(self):
+        self._set_headers()
+    def do_GET(self):
+        self._set_headers()
+        print self.path
+        print parse_qs(self.path[2:])
+        self.wfile.write("<html><body><h1>Get Request Received!</h1></body></html>")
+    def do_POST(self):
+        self._set_headers()
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD': 'POST'}
+        )
+        print form.getvalue("from_user")
+        print form.getvalue("to_user")
+        print form.getvalue("message")
+        self.wfile.write("<html><body><h1>POST Request Received!</h1></body></html>")
+
+def run(server_class=HTTPServer, handler_class=GP, port=8088):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print 'Server running at localhost:8088...'
+    httpd.serve_forever()
+
+run()
